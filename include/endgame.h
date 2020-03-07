3d9ad53a8af4009a76a6960c037a2b99516f13de
Colin Rocks <colinrocks@pm.me>
2020-03-07 19:57:00 +0000

    Upload New File

---
+++ include/endgame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <ctre/Phoenix.h>
+#include "ControllerClass.h"
+
+class endgame
+{
+
+public:
+    endgame();
+    void endgameControl(double, double);
+
+private:
+    //VictorSPX *endgameMaster;
+    //VictorSPX *endgameSlave;
+
+};
